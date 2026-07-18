6d8e5a56a47a965175264bf231aa931e9163d85e
joker2305 <550556076@qq.com>
2026-07-18 14:48:36 +0800

    fix(vscode): 修复微服务 Dashboard 启动配置

--- backend/medconsult-common/common-mq/src/test/java/com/medconsult/common/mq/MqTestApplication.java
+++ backend/medconsult-common/common-mq/src/test/java/com/medconsult/common/mq/MqTestApplication.java
@@ -1,7 +1,6 @@
 package com.medconsult.common.mq;
 
 import org.mybatis.spring.annotation.MapperScan;
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
@@ -10,7 +9,4 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 @MapperScan("com.medconsult.common.mq")
 public class MqTestApplication {
-    public static void main(String[] args) {
-        SpringApplication.run(MqTestApplication.class, args);
-    }
 }
